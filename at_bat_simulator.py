6767919681f05078ef056067d09efc9b25392570
Ryan Behnke <rjbehnke2@gmail.com>
2018-07-25 16:50:24 -0500

    Testing batter data

--- at_bat_simulator.py
+++ at_bat_simulator.py
@@ -1,9 +1,5 @@
 from pybaseball import batting_stats_range
 
-# retrieve all players' batting stats for the month of May, 2017
-data = batting_stats_range("2017-05-01", "2017-05-28")
+hitter_data = batting_stats_range("2015-01-01", "2018-01-01")
 
-# retrieve batting stats for only August 24, 2016
-#data = batting_stats_range("2016-08-24",)
-
-print(data)
+print(hitter_data)
