f7c9b9e4e4bff18b5f4e82f17cd1c79e5145df1d
Ryan Behnke <rjbehnke2@gmail.com>
2018-07-25 17:05:19 -0500

    Testing pitcher data

--- at_bat_simulator.py
+++ at_bat_simulator.py
@@ -1,6 +1,8 @@
 from pybaseball import batting_stats
+from pybaseball import pitching_stats
 
-hitter_data = batting_stats(2015, 2017)
+#hitter_data = batting_stats(2015, 2017)
+pitcher_data = pitching_stats(2015, 2017)
 
 
-print(hitter_data)
+print(pitcher_data)
