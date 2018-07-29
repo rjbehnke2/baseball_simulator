12086c720934353b758b25bb484b07177d352d9b
Ryan Behnke <rjbehnke2@gmail.com>
2018-07-29 17:25:43 -0500

    Remove unneeded class; Update class naming

---
+++ AtBatSimulator.py
@@ -0,0 +1,8 @@
+from pybaseball import batting_stats
+from pybaseball import pitching_stats
+
+#hitter_data = batting_stats(2015, 2017)
+pitcher_data = pitching_stats(2015, 2017)
+
+
+print(pitcher_data)
