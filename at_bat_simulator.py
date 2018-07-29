74a5bb8ce8599b96e10f24830b308bdaa405f2d6
Ryan Behnke <rjbehnke2@gmail.com>
2018-07-29 17:27:55 -0500

    Whoops, forgot to remove the classes

--- at_bat_simulator.py
+++
@@ -1,8 +0,0 @@
-from pybaseball import batting_stats
-from pybaseball import pitching_stats
-
-#hitter_data = batting_stats(2015, 2017)
-pitcher_data = pitching_stats(2015, 2017)
-
-
-print(pitcher_data)
